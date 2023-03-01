86a97261c439f67e3754add6d8d9007e1186357e
Mike Pirog <mike@lando.dev>
2023-03-01 11:48:32 -0500

    comscan has detected new scanner test (#30)
    * disable legacy scanner by default
    
    * first pass on new url scanning
    
    * second pass on new url scanning
    
    * switch GA to use extended core features for faster testing
    
    * switch GA to use extended core features for faster testing part 2
    
    * switch GA to use extended core features for faster testing part 3
    
    * rework restart and rebuild for new scanner tech
    
    * restart tests

--- plugins/lando-core/tasks/restart.js
+++ plugins/lando-core/tasks/restart.js
@@ -3,6 +3,8 @@
 const _ = require('lodash');
 const utils = require('./../lib/utils');
 
+const {Manager} = require('listr2');
+
 module.exports = lando => {
   return {
     command: 'restart',
@@ -10,15 +12,48 @@ module.exports = lando => {
     run: options => {
       // Try to get our app
       const app = lando.getApp(options._app.root);
+      const legacyScanner = _.get(lando, 'config.scanner', true) === 'legacy';
+
       // Restart it if we can!
       if (app) {
         console.log(lando.cli.makeArt('appRestart', {name: app.name, phase: 'pre'}));
         // Normal
         return app.restart().then(() => {
           const type = !_.isEmpty(app.warnings) ? 'report' : 'post';
-          console.log(lando.cli.makeArt('appStart', {name: app.name, phase: type, warnings: app.warnings}));
-          console.log(lando.cli.formatData(utils.startTable(app), {format: 'table'}, {border: false}));
-          console.log('');
+          const phase = legacyScanner ? `${type}_legacy` : type;
+          console.log(lando.cli.makeArt('appStart', {name: app.name, phase, warnings: app.warnings}));
+          console.log(lando.cli.formatData(utils.startTable(app, {legacyScanner}), {format: 'table'}, {border: false}));
+
+          if (!legacyScanner) {
+            const scanTasks = _(app.checks)
+              .filter(checks => checks.type === 'url-scan')
+              .groupBy('service')
+              .map((tasks, name) => ({
+                title: lando.cli.chalk.cyan(`${_.upperCase(name)} URLS`),
+                task: (ctx, task) => {
+                  const subtasks = _(tasks).map(subtask => lando.cli.check2task(subtask)).value();
+                  return task.newListr(subtasks, {concurrent: true, exitOnError: false});
+                },
+              }))
+              .value();
+
+            // listr things
+            // if verbose or debug mode is on then use the verbose renderer
+            const renderer = options.debug || options.verbose > 0 ? 'verbose' : lando.cli.getRenderer();
+            const rendererOptions = {collapse: false, level: 1, suffixRetries: false, showErrorMessage: false};
+            const listrOptions = {renderer, concurrent: true, showErrorMessage: false, rendererOptions};
+            const tasks = new Manager(listrOptions);
+            tasks.add(scanTasks);
+
+            // run the listr
+            return tasks.runAll()
+            .then(stuff => {
+              console.log('');
+            })
+            .catch(error => {
+              throw error;
+            });
+          } else console.log(' ');
         })
         // Provide help if there is an error
         .catch(err => {
