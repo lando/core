d5aec495f4d6ec51256fe0a6c38d0734ceaf6949
Mike Pirog <mike@lando.dev>
2023-04-07 08:43:31 -0400

    modify new scanner default redirect behavior to be the same as it was previously, make redirects configurable

--- plugins/lando-scanner/app.js
+++ plugins/lando-scanner/app.js
@@ -6,47 +6,45 @@ const debug = require('debug')('@lando/core:scanner');
 const http = require('http');
 const https = require('https');
 
-const request = () => {
-  const axios = require('axios');
-  // @todo: is it ok to turn redirects off here?
-  // if we don't we get an error every time http tries to redirect to https
-  return axios.create({
-    maxRedirects: 3,
+const request = (maxRedirects = 0) => {
+  return require('axios').create({
+    maxRedirects,
     httpAgent: new http.Agent({family: 4}),
     httpsAgent: new https.Agent({rejectUnauthorized: false, family: 4}),
   });
 };
 
 // @TODO: add some debugging? or only in CLI?
-const scan = (baseURL, {okCodes = [], log = debug, path = '/', timeout = 3000} = {}) => request()
-  .get(path, {baseURL, timeout})
-  .then(response => {
-    response.lando = {code: _.get(response, 'status', 'unknown'), text: _.get(response, 'statusText', 'unknown')};
-    log('scan of %o passed with %o %o', `${baseURL}${path}`, response.lando.code, response.lando.text);
-    return response;
-  })
-  .catch(error => {
-    // get teh response from the error
-    const {response} = error;
-    // standardize for lando, first try to get http code
-    error.lando = {code: _.get(response, 'status', 'unknown'), text: _.get(response, 'statusText', 'unknown')};
-    // if its unknown then try a few other scenarios eg timeout
-    if (error.lando.code === 'unknown' && _.startsWith(error.message, 'timeout')) error.lando.code = 'TIMEOUT';
-    // if still unknown try to grab the code
-    if (error.lando.code === 'unknown') error.lando.code = _.get(error, 'code', 'unknown');
+const scan = (baseURL, {okCodes = [], maxRedirects = 0, log = debug, path = '/', timeout = 3000} = {}) => {
+  return request(maxRedirects).get(path, {baseURL, timeout})
+    .then(response => {
+      response.lando = {code: _.get(response, 'status', 'unknown'), text: _.get(response, 'statusText', 'unknown')};
+      log('scan of %o passed with %o %o', `${baseURL}${path}`, response.lando.code, response.lando.text);
+      return response;
+    })
+    .catch(error => {
+      // get teh response from the error
+      const {response} = error;
+      // standardize for lando, first try to get http code
+      error.lando = {code: _.get(response, 'status', 'unknown'), text: _.get(response, 'statusText', 'unknown')};
+      // if its unknown then try a few other scenarios eg timeout
+      if (error.lando.code === 'unknown' && _.startsWith(error.message, 'timeout')) error.lando.code = 'TIMEOUT';
+      // if still unknown try to grab the code
+      if (error.lando.code === 'unknown') error.lando.code = _.get(error, 'code', 'unknown');
 
-    // if an OK code then also pass
-    if (_.includes(okCodes, error.lando.code)) {
-      log('scan of %o passed with ok code', `${baseURL}${path}`, error.lando.code);
-      return {lando: error.lando};
-    }
+      // if an OK code then also pass
+      if (_.includes(okCodes, error.lando.code)) {
+        log('scan of %o passed with ok code', `${baseURL}${path}`, error.lando.code);
+        return {lando: error.lando};
+      }
 
-    // if we get here then debug full error?
-    log('scan of %o failed with %o %o', `${baseURL}${path}`, error.lando.code, error.lando.text);
+      // if we get here then debug full error?
+      log('scan of %o failed with %o %o', `${baseURL}${path}`, error.lando.code, error.lando.text);
 
-    // throw
-    throw error;
-  });
+      // throw
+      throw error;
+    });
+};
 
 module.exports = async (app, lando) => {
   // ensure that checks exists and is an array
@@ -70,6 +68,7 @@ module.exports = async (app, lando) => {
           title: url,
           args: [url, {
             okCodes: _.get(app, `config.services.${service.service}.scanner.okCodes`, []),
+            maxRedirects: _.get(app, `config.services.${service.service}.scanner.maxRedirects`, 0),
             timeout: _.get(app, `config.services.${service.service}.scanner.timeout`, 10000),
             log: app.log.debug,
             path: _.get(app, `config.services.${service.service}.scanner.path`, '/'),
