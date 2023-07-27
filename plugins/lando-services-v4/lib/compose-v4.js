870273d4a737600ef3422f51af52a74dc8a6b59c
Mike Pirog <mike@lando.dev>
2023-07-27 13:30:44 -0400

    reclassify classes

--- plugins/lando-services-v4/lib/compose-v4.js
+++ plugins/lando-services-v4/lib/compose-v4.js
@@ -2,14 +2,28 @@
 
 const _ = require('lodash');
 
-// adds required methods to ensure the lando v3 debugger can be injected into v4 things
 class ComposeServiceV4 {
-  constructor(id, info = {}, buildContext = {}, ...compose) {
+  constructor(id, config = {}, ctx = {}) {
     this.id = id;
-    this.buildContext = buildContext;
-    this.compose = {id, info, data: _(compose).map(element => _.merge({}, element, {version: '3.6'})).value()};
-    this.info = info;
+    this.config = config;
+
+    this.buildContext = {};
+    this.data = {};
+    this.composeData = [];
+    this.info = {};
   };
+
+  dump() {
+    return {
+      buildContext: this.buildContext,
+      compose: {
+        id: this.id,
+        info: this.info,
+        data: _(this.composeData).map(element => _.merge({}, element, {version: '3.6'})).value(),
+      },
+      info: this.info,
+    };
+  }
 };
 
 module.exports = ComposeServiceV4;
