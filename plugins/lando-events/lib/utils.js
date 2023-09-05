62e6839e38d482bf2eb7eb45f3064a296c609d2f
Mike Pirog <mike@lando.dev>
2023-09-05 13:51:00 -0400

    update event tests for new v4 stuff and general improvements

--- plugins/lando-events/lib/utils.js
+++ plugins/lando-events/lib/utils.js
@@ -6,16 +6,21 @@ const getUser = require('./../../../lib/utils').getUser;
 
 // Helper to find the default service
 const getDefaultService = (data = {}, defaultService = 'appserver') => {
-  if (_.has(data, 'service')) {
-    if (_.startsWith(data.service, ':')) {
-      const option = _.trimStart(data.service, ':');
-      return _.get(data, `options.${option}.default`, defaultService);
-    } else {
-      return _.get(data, 'service');
-    }
-  } else {
-    return defaultService;
+  // if this is an event built on a service-dynamic command
+  if (_.has(data, 'service') && _.startsWith(data.service, ':')) {
+    // get the option name
+    const option = _.trimStart(data.service, ':');
+    // get the argv
+    const argv = _.get(data, 'app.config.argv', {});
+    // return the argv of the option or its default value
+    return _.has(argv, option) ? argv[option] : _.get(data, `options.${option}.default`, defaultService);
   }
+
+  // or if the service is explicitly set by a tooling command
+  if (_.has(data, 'service')) return _.get(data, 'service');
+
+  // otherwise just return the default
+  return defaultService;
 };
 
 // Helper to find a command
@@ -36,6 +41,14 @@ exports.events2Runz = (cmds, app, data = {}) => _.map(cmds, cmd => {
   // Discover the service
   const command = getCommand(cmd);
   const service = getService(cmd, data, app._defaultService);
+
+  // try to get a list of v4 services a few ways, we have to look at different places because the event could
+  // run at various points in the bootstrap
+  const v4s = _([
+    _(app.info).filter(service => service.api === 4).map('service').value(),
+    _.get(app, 'v4.servicesList', []),
+  ]).flatten().compact().uniq().value();
+
   // Validate the service if we can
   // @NOTE fast engine runs might not have this data yet
   if (app.services && !_.includes(app.services, service)) {
@@ -48,7 +61,7 @@ exports.events2Runz = (cmds, app, data = {}) => _.map(cmds, cmd => {
     cmd: ['/bin/sh', '-c', _.isArray(command) ? command.join(' ') : command],
     compose: app.compose,
     project: app.project,
-    api: _.includes(_.get(app, 'v4.servicesList', []), service) ? 4 : 3,
+    api: _.includes(v4s, service) ? 4 : 3,
     opts: {
       cstdio: ['inherit', 'pipe', 'pipe'],
       mode: 'attach',
