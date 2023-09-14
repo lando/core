e3eaa75cac5863f48b5ed739a4a8decda512245f
Mike Pirog <mike@lando.dev>
2023-09-14 11:29:55 -0400

    start dockerfile validation, and lando-v4 work

--- plugins/lando-services-v4/utils/parse-config.js
+++ plugins/lando-services-v4/utils/parse-config.js
@@ -14,7 +14,6 @@ const getApiVersion = (version = 3) => {
 
 // adds required methods to ensure the lando v3 debugger can be injected into v4 things
 module.exports = services => _(services)
-  // Arrayify and strip v3-isms out of the config
   .map((service, name) => _.merge({}, {
     name,
     api: getApiVersion(service.api),
@@ -26,13 +25,6 @@ module.exports = services => _(services)
     },
     primary: service.primary || false,
     scanner: service.scanner || false,
+    type: service.type || 'l337',
   }))
-  // ensure api is set to something valid
-  .map(service => _.merge({}, service, {api: getApiVersion(service.api)}))
-  // set v4 base type if applicable
-  .map(service => _.merge({}, {type: service.api === 4 ? 'l337' : undefined}, service))
-  // Filter out any services without a type, this implicitly assumes these
-  // services are "managed" by lando eg their type/version details are provided
-  // by another service
-  .filter(service => !_.isNil(service.type))
   .value();
