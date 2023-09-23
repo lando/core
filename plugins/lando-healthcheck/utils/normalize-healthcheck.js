5bbaffc49c7fd7b35cdf0f791a703bebe052d97f
Mike Pirog <mike@lando.dev>
2023-09-23 13:59:09 -0400

    make sure that arrayified healthchecks end up in the right place as well (#57)

--- plugins/lando-healthcheck/utils/normalize-healthcheck.js
+++ plugins/lando-healthcheck/utils/normalize-healthcheck.js
@@ -5,6 +5,8 @@ const defaults = {delay: 1000, retry: 25, user: 'root'};
 module.exports = healthcheck => {
   // if healthcheck is a string then objectify
   if (typeof healthcheck === 'string') healthcheck = {command: healthcheck};
+  // ditto if its an array
+  else if (Array.isArray(healthcheck)) healthcheck = {command: healthcheck};
   // allow cmd shorthand
   if (!healthcheck.command && healthcheck.cmd) healthcheck.command = healthcheck.cmd;
   // merge in defaults and return
