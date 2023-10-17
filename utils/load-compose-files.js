13108f6a4afa4514a09d1430b8fe8b3735950859
Mike Pirog <mike@lando.dev>
2023-10-17 11:59:45 -0400

    hardcore5a

--- utils/load-compose-files.js
+++ utils/load-compose-files.js
@@ -4,6 +4,6 @@ const _ = require('lodash');
 const Yaml = require('./../lib/yaml');
 const yaml = new Yaml();
 
-module.exports = data => (files, dir) => _(require('./normalize-files')(files, dir))
+module.exports = (files, dir) => _(require('./normalize-files')(files, dir))
   .map(file => yaml.load(file))
   .value();
