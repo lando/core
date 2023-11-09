c1860f1d536f8b5f2af319ca3dc41ca6e2768ddd
Mike Pirog <mike@lando.dev>
2023-11-09 09:45:06 -0500

    make sure all debug logging is going to the right fd, conditionalize setup build engine for darwin only for now

--- hooks/lando-setup-build-engine.js
+++ hooks/lando-setup-build-engine.js
@@ -72,83 +72,87 @@ module.exports = async (lando, options) => {
   const buildEngine = process.platform === 'linux' ? 'docker-engine' : 'docker-desktop';
   const install = version ? `v${version}` : `build ${build}`;
 
-  options.tasks.push({
-    title: `Downloading build engine`,
-    id: 'setup-build-engine',
-    description: `@lando/build-engine (${buildEngine})`,
-    required: true,
-    version: `${buildEngine} ${install}`,
-    hasRun: async () => {
-      // start by looking at the engine install status
-      // @NOTE: is this always defined?
-      if (lando.engine.dockerInstalled === false) return false;
+  // only install on darwin for now
+  // @TODO: add support for linux/windows
+  if (process.platform === 'darwin') {
+    options.tasks.push({
+      title: `Downloading build engine`,
+      id: 'setup-build-engine',
+      description: `@lando/build-engine (${buildEngine})`,
+      required: true,
+      version: `${buildEngine} ${install}`,
+      hasRun: async () => {
+        // start by looking at the engine install status
+        // @NOTE: is this always defined?
+        if (lando.engine.dockerInstalled === false) return false;
 
-      // if we get here let's make sure the engine is on
-      try {
-        await lando.engine.daemon.up();
-        const BuildEngine = require('../components/docker-engine');
-        const bengine = new BuildEngine(lando.config.buildEngine, {debug});
-        await bengine.info();
-        return true;
-      } catch (error) {
-        lando.log.debug('docker install task has not run %j', error);
-        return false;
-      }
-    },
-    canRun: async () => {
-      // throw if we cannot resolve a semantic version to a buildid
-      if (!build) throw new Error(`Could not resolve ${install} to an installable version!`);
-      // throw error if not online
-      if (!await require('is-online')()) throw new Error('Cannot detect connection to internet!');
-      // throw if user is not an admin
-      if (!await require('../utils/is-admin-user')()) {
-        throw new Error([
-          `User "${os.userInfo().username}" does not have permission to install the build engine!`,
-          'Contact your system admin for permission and then rerun setup.',
-        ].join(os.EOL));
-      }
-
-      return true;
-    },
-    task: async (ctx, task) => {
-      try {
-        // path to installer script
-        const installerScript = path.join(lando.config.userConfRoot, 'scripts', 'install-docker-desktop.sh');
-        const username = os.userInfo().username;
-
-        // download the installer
-        ctx.download = await downloadDockerDesktop(getEngineDownloadUrl(build), {ctx, debug, task});
-
-        // prompt for password if interactive
-        if (require('is-interactive')) {
-          ctx.password = await task.prompt({
-            type: 'password',
-            name: 'password',
-            message: `Enter computer password for ${username} to install build engine`,
-            validate: async (input, state) => {
-              const options = {debug, ignoreReturnCode: true, password: input};
-              const response = await require('../utils/run-elevated')(['echo', 'hello there'], options);
-              if (response.code !== 0) return response.stderr;
-              return true;
-            },
-          });
+        // if we get here let's make sure the engine is on
+        try {
+          await lando.engine.daemon.up();
+          const BuildEngine = require('../components/docker-engine');
+          const bengine = new BuildEngine(lando.config.buildEngine, {debug});
+          await bengine.info();
+          return true;
+        } catch (error) {
+          lando.log.debug('docker install task has not run %j', error);
+          return false;
+        }
+      },
+      canRun: async () => {
+        // throw if we cannot resolve a semantic version to a buildid
+        if (!build) throw new Error(`Could not resolve ${install} to an installable version!`);
+        // throw error if not online
+        if (!await require('is-online')()) throw new Error('Cannot detect connection to internet!');
+        // throw if user is not an admin
+        if (!await require('../utils/is-admin-user')()) {
+          throw new Error([
+            `User "${os.userInfo().username}" does not have permission to install the build engine!`,
+            'Contact your system admin for permission and then rerun setup.',
+          ].join(os.EOL));
         }
 
-        // run install command
-        task.title = `Installing build engine ${color.dim('(this may take a minute)')}`;
-        await require('../utils/run-elevated')([installerScript, ctx.download.dest, username], {
-          debug,
-          password: ctx.password,
-        });
+        return true;
+      },
+      task: async (ctx, task) => {
+        try {
+          // path to installer script
+          const installerScript = path.join(lando.config.userConfRoot, 'scripts', 'install-docker-desktop.sh');
+          const username = os.userInfo().username;
 
-        // finish up
-        ctx.run++;
-        task.title = 'Installed build engine to /Applications/Docker.app';
+          // download the installer
+          ctx.download = await downloadDockerDesktop(getEngineDownloadUrl(build), {ctx, debug, task});
 
-      // push any errorz
-      } catch (error) {
-        ctx.errors.push(error);
-      }
-    },
-  });
+          // prompt for password if interactive
+          if (require('is-interactive')) {
+            ctx.password = await task.prompt({
+              type: 'password',
+              name: 'password',
+              message: `Enter computer password for ${username} to install build engine`,
+              validate: async (input, state) => {
+                const options = {debug, ignoreReturnCode: true, password: input};
+                const response = await require('../utils/run-elevated')(['echo', 'hello there'], options);
+                if (response.code !== 0) return response.stderr;
+                return true;
+              },
+            });
+          }
+
+          // run install command
+          task.title = `Installing build engine ${color.dim('(this may take a minute)')}`;
+          await require('../utils/run-elevated')([installerScript, ctx.download.dest, username], {
+            debug,
+            password: ctx.password,
+          });
+
+          // finish up
+          ctx.run++;
+          task.title = 'Installed build engine to /Applications/Docker.app';
+
+        // push any errorz
+        } catch (error) {
+          ctx.errors.push(error);
+        }
+      },
+    });
+  }
 };
