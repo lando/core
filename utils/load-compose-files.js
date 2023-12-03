b88048d1742ae50a83c078c182be62c4cf200d23
florianPat <florian.patruck@gmx.de>
2023-12-03 12:14:09 +0100

    fix(compose): Fix compose file path resolution as it is copied into the lando directory
    Refs: https://github.com/lando/lando/issues/3373

--- utils/load-compose-files.js
+++ utils/load-compose-files.js
@@ -2,8 +2,19 @@
 
 const _ = require('lodash');
 const Yaml = require('./../lib/yaml');
+const path = require('path');
 const yaml = new Yaml();
+const fs = require('fs');
 
-module.exports = (files, dir) => _(require('./normalize-files')(files, dir))
-  .map(file => yaml.load(file))
-  .value();
+// NOTE: This just runs `docker compose --project-directory ${dir} config -f ${files} --output ${outputPaths}` to
+// make all paths relative to the lando config root
+module.exports = (files, dir, landoComposeConfigDir, outputConfigFunction) => {
+  const composeFilePaths = _(require('./normalize-files')(files, dir)).value();
+  const outputFile = path.join(landoComposeConfigDir, 'resolved-compose-config.yml');
+
+  outputConfigFunction(composeFilePaths, outputFile);
+  const result = yaml.load(outputFile);
+  fs.unlinkSync(outputFile);
+
+  return result;
+};
