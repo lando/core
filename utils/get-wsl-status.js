5ee776c8455348e29f63b1d8aa86cf3dda0411f2
Mike Pirog <mike@lando.dev>
2024-03-15 10:07:19 -0400

    DEBUG WIN17

--- utils/get-wsl-status.js
+++ utils/get-wsl-status.js
@@ -13,24 +13,27 @@ const defaults = {
 module.exports = async (options = {}) => {
   const args = ['-Command', 'wsl --status'];
   const opts = merge({}, defaults, options);
+  const {debug} = opts;
   const {code, stdout} = await require('./run-command')('powershell', args, opts);
-  console.log(code, stdout);
-  console.log(opts.env);
 
   // if code is non zero we can return uninstalled
   if (code !== 0) return {installed: false, features: false, version: undefined};
 
   // otherwise lets try to sus things out by first making sure we have something parseable
   const data = !stdout.includes('Default Version') ? Buffer.from(stdout, 'utf8').toString('utf16le') : stdout;
-  console.log(data);
 
   // try to get version
   const versionLine = data.split('\n').filter(line => line.includes('Default Version'))[0];
-  console.log(versionLine);
+  const versionString = versionLine ?? '';
+  const version = typeof versionString.split(':')[1] === 'string' ? versionString.split(':')[1].trim() : undefined;
+
+  // debug
+  debug('discovered wsl version %o with code %o', version, code);
 
   return {
-    installed: true,
+    installed: (code === 0 || code === 1) && version !== undefined,
     features: !data.includes('"Virtual Machine Platform"') && !data.includes('"Windows Subsystem for Linux"'),
+    version,
   };
 };
 
