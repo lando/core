63ae625406dd7268f2700a52746270bf05177f1c
Mike Pirog <mike@lando.dev>
2024-04-10 11:33:08 -0400

    update docker components (#143)
    * update docker components
    
    * remove stray debug line
    
    * DEBUG 1
    
    * roll back a bit so we can test plugin refresh
    
    * roll back a bit so we can test plugin refresh part 2
    
    * roll back a bit so we can test plugin refresh part 3
    
    * rework tests for proper setup dogfood

--- hooks/lando-setup-build-engine-win32.js
+++ hooks/lando-setup-build-engine-win32.js
@@ -8,6 +8,7 @@ const {color} = require('listr2');
 const {nanoid} = require('nanoid');
 
 const buildIds = {
+  '4.29.0': '145265',
   '4.28.0': '139021',
   '4.27.2': '137060',
   '4.27.1': '136059',
