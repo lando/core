4e12a7135eb2fa4e3534dc2234dfe5fad3b87fa4
Mike Pirog <mike@lando.dev>
2024-04-23 09:36:30 -0400

    push failing condition to GHA part5

--- utils/generate-build-script.js
+++ utils/generate-build-script.js
@@ -4,15 +4,16 @@
 module.exports = (contents, user, gid, socket, mount = '/app') => `
 #!/bin/sh
 # clean up and setup ssh-auth
-if [ -S "${socket}" ]; then
-  rm -rf /run/ssh-${user}.sock
-  sudo socat UNIX-LISTEN:/run/ssh-${user}.sock,fork,user=pirog,group=${gid},mode=777 UNIX-CONNECT:${socket} &
-fi
+# @TODO: only run if socket exists?
+# if [ -S "${socket}" ]; then
+echo "socat"
+rm -rf /run/ssh-${user}.sock
+sudo socat UNIX-LISTEN:/run/ssh-${user}.sock,fork,user=${user},group=${gid},mode=777 UNIX-CONNECT:${socket} &
 
 # temp stuff for demo purposes
-if command -v git > /dev/null 2>&1; then
-  git config --global --add safe.directory ${mount}
-fi
+# @TODO: only run if git exists?
+# if command -v git > /dev/null 2>&1; then
+git config --global --add safe.directory ${mount}
 
 ${contents}
 `;
