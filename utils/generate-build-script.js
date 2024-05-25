6aac8d366be2ced485a855d8ed9d880e5ed4748f
Mike Pirog <mike@lando.dev>
2024-05-25 07:16:34 -0400

    improve some lando4 service things part 2

--- utils/generate-build-script.js
+++ utils/generate-build-script.js
@@ -46,30 +46,3 @@ fi
 
 ${contents}
 `;
-
-/*
-# Timeout period in seconds
-TIMEOUT=30
-# Time interval between checks in seconds
-INTERVAL=1
-# Start time
-START_TIME=$(date +%s)
-# wait until socket is ready
-while true; do
-  # Check if ssh-agent is running
-  if ssh-add -l > /dev/null 2>&1; then
-    break
-  fi
-
-  # Check if the timeout period has been reached
-  CURRENT_TIME=$(date +%s)
-  ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
-  if [ $ELAPSED_TIME -ge $TIMEOUT ]; then
-    echo "Timeout reached. ssh-agent is not ready."
-    exit 1
-  fi
-
-  # Wait for the specified interval before checking again
-  sleep $INTERVAL
-done
-*/
