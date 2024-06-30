7ce92c8beac687be022fc028c7f1a65bf19cbe39
Mike Pirog <mike@lando.dev>
2024-06-29 22:58:27 -0400

    rework orchestration file dumping to be more dynamic in v4 part 5

--- hooks/app-purge-compose-cache.js
+++ hooks/app-purge-compose-cache.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = async (app, lando) => {
-  // wipe the compose cache
-  lando.cache.remove(app.composeCache);
   // reset v4 service state
   for (const service of app?.v4?.services ?? []) {
     service.info = undefined;
   }
+  // wipe the compose cache
+  lando.cache.remove(app.composeCache);
 };
