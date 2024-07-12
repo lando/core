a6123c38bb8d8f5f4110dd8b07bd5c5253a93111
Mike Pirog <mike@lando.dev>
2024-07-12 11:10:11 -0400

    first pass on proper execy backgrounding

--- utils/build-docker-exec.js
+++ utils/build-docker-exec.js
@@ -24,6 +24,16 @@ const getExecOpts = (docker, datum) => {
     exec.push('--env');
     exec.push(`${key}=${value}`);
   });
+
+  // Assess the intention to detach
+  if (datum.cmd[0] === '/etc/lando/exec.sh' && datum.cmd[datum.cmd.length - 1] === '&') {
+    datum.cmd.pop();
+    exec.push('--detach');
+  } else if (datum.cmd[0] === '/bin/sh' && datum.cmd[1] === '-c' && datum.cmd[2].endsWith('&')) {
+    datum.cmd[2] = datum.cmd[2].slice(0, -1).trim();
+    exec.push('--detach');
+  }
+
   // Add id
   exec.push(datum.id);
   return exec;
@@ -33,7 +43,6 @@ module.exports = (injected, stdio, datum = {}) => {
   // Depending on whether injected is the app or lando
   const dockerBin = injected.config.dockerBin || injected._config.dockerBin;
   const opts = {mode: 'attach', cstdio: stdio};
-
   // Run run run
   return injected.shell.sh(getExecOpts(dockerBin, datum).concat(datum.cmd), opts);
 };
