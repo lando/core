50787352bb886442f1d9b1e275f54d51f7d15358
Mike Pirog <mike@lando.dev>
2024-07-13 08:12:29 -0400

    linux system-ca loading

--- utils/get-system-cas.js
+++ utils/get-system-cas.js
@@ -1,14 +1,23 @@
 'use strict';
 
 module.exports = (format = 'fingerprint') => {
+  const fingerprints = [];
+
   switch (process.platform) {
     case 'darwin':
       return require('mac-ca').get({format});
     case 'linux':
-      return [];
+      const {systemCertsSync} = require('system-ca');
+
+      for (const cert of systemCertsSync()) {
+        try {
+          fingerprints.push(require('./get-fingerprint')(cert));
+        } catch {}
+      }
+
+      return fingerprints;
     case 'win32':
       const winCA = require('win-ca');
-      const fingerprints = [];
 
       for (const cert of [...winCA({generator: true, store: ['root'], format: winCA.der2.pem})]) {
         try {
