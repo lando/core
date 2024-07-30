38ec759feca3dd935c50b6cdf51c7329ff736bc3
Mike Pirog <mike@lando.dev>
2024-07-30 07:56:11 -0400

    try out simpler execer to see what breaks

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -3,15 +3,5 @@
 # load in any dynamically set envvars
 source /etc/lando/environment
 
-# List of characters that exec cannot handle directly
-SPECIAL_CHARS='&&|\||;|\$|\`|>|>>|<|2>|&>'
-
-# Join all arguments into a single string
-CMD="$*"
-
-# Check if the command contains any special characters
-if echo "$CMD" | grep -qE "$SPECIAL_CHARS"; then
-  exec sh -c "$CMD"
-else
-  exec "$@"
-fi
+debug "$@ with $# args"
+exec "$@"
