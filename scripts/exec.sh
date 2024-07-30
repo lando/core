9e3de5197e109bc692664804c517f9c759bf4927
Mike Pirog <mike@lando.dev>
2024-07-30 09:24:49 -0400

    this will begin to set things right

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -3,5 +3,19 @@
 # load in any dynamically set envvars
 source /etc/lando/environment
 
+# we need a holder for our expanded args
+args=()
+
+# iterate and expand any variables
+# @NOTE: does this have other unintended non-variable consequences?
+for arg in "$@"; do
+  earg=$(eval echo "$arg")
+  args+=("$earg")
+done
+
+# replace "$@" with args
+set -- "${args[@]}"
+
+# DO IT!
 debug "$@ with $# args"
 exec "$@"
