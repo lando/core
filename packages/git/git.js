87e92095e0f2f55abc757d18cbdaab13b210ed78
Mike Pirog <mike@lando.dev>
2024-09-04 09:43:50 -0400

    fix hook system, git safe dir, rebase docs .lando.yml on lando 4 service

--- packages/git/git.js
+++ packages/git/git.js
@@ -5,9 +5,8 @@ const path = require('path');
 module.exports = async service => {
   service.addHookFile(path.join(__dirname, 'install-git.sh'), {hook: 'boot'});
   service.addHookFile(`
-    # temp stuff for demo purposes
     if command -v git > /dev/null 2>&1; then
-      git config --global --add safe.directory ${service.appMount}
+      git config --system --add safe.directory ${service.appMount}
     fi
-  `, {stage: 'app', hook: 'internal-root', id: 'git-safe'});
+  `, {hook: 'tooling', priority: 0});
 };
