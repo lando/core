b4f517ca9436a4126213b24e0665bedc9a7136aa
florianPat <florian.patruck@gmx.de>
2024-09-09 21:33:15 +0200

    feat(volumes): Use the lando proxy dir as the config volume and therefore do not copy the files over

--- utils/load-compose-files.js
+++ utils/load-compose-files.js
@@ -9,10 +9,16 @@ const remove = require('./remove');
 
 // This just runs `docker compose --project-directory ${dir} config -f ${files} --output ${outputPaths}` to
 // make all paths relative to the lando config root
-module.exports = async (files, dir, landoComposeConfigDir, outputConfigFunction) => {
+module.exports = async (files, dir, landoComposeConfigDir = undefined, outputConfigFunction = undefined) => {
   const composeFilePaths = _(require('./normalize-files')(files, dir)).value();
   if (_.isEmpty(composeFilePaths)) {
     return {};
+  }
+
+  if (undefined === outputConfigFunction) {
+    return _(composeFilePaths)
+      .map(file => yaml.load(file))
+      .value();
   }
 
   const outputFile = path.join(landoComposeConfigDir, 'resolved-compose-config.yml');
