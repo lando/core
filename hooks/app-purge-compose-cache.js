30b0fb18d17b19585d47abd9bee4cad10693b417
Mike Pirog <mike@lando.dev>
2024-09-18 10:47:16 -0400

    improve cleanup of dangling compose files on compose cache purging

--- hooks/app-purge-compose-cache.js
+++ hooks/app-purge-compose-cache.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async (app, lando) => {
   // reset v4 service state
   for (const service of app?.v4?.services ?? []) service.info = undefined;
 
   // reset tooling overrides
   app._coreToolingOverrides = {};
+
+  // remove compose cache danglerz
+  fs.readdirSync(app._dir)
+    .map(dangler => path.join(app._dir, dangler))
+    .filter(dangler => !app.compose.includes(dangler))
+    .map(dangler => {
+      try {
+        fs.unlinkSync(dangler);
+      } catch {
+        app.log.info('Could not remove dangling compose file %s', dangler);
+        return dangler;
+      }
+    });
 
   // wipe the compose cache
   lando.cache.remove(app.composeCache);
