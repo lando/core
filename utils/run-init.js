767fc7fc7d3ce31c417c0883ffb67dad87e18a93
Florian Patruck <florian.patruck@hdnet.de>
2024-10-19 21:10:05 +0200

    feat(init): Remove init compose after execution

--- utils/run-init.js
+++ utils/run-init.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const remove = require('../utils/remove');
+const path = require('path');
+
 // Helper to kill a run
 const killRun = config => ({
   id: config.id,
@@ -13,7 +16,9 @@ const killRun = config => ({
 
 // adds required methods to ensure the lando v3 debugger can be injected into v4 things
 module.exports = (lando, run) => lando.engine.run(run).catch(err => {
+  return lando.Promise.reject(err);
+}).finally(() => {
   return lando.engine.stop(killRun(run))
   .then(() => lando.engine.destroy(killRun(run)))
-  .then(() => lando.Promise.reject(err));
+  .then(() => remove(path.dirname(run.compose[0])));
 });
