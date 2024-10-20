5464bd3eeab761eab3fbc7140b6b66ff56bcd651
Mike Pirog <mike@lando.dev>
2024-10-20 12:46:11 -0400

    add in some more lingering utils

---
+++ utils/prettify.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const os = require('os');
+
+/*
+ * prettify object data for better tabular output
+ * @TODO: is it ok to assume that all data in an array will be the same type?
+ */
+module.exports = (data, {arraySeparator = ', '} = {}) => {
+  // if undefined then just return an empty string
+  if (data === undefined) return '';
+
+  // handle arrays differently
+  if (Array.isArray(data)) {
+    // join lists of strings together
+    if (typeof data[0] === 'string') {
+      return data.join(arraySeparator);
+    }
+
+    // print arrays of objects nicely
+    if (data[0] && typeof data[0] === 'object' && Object.keys(data[0]).length > 0) {
+      return data.map(item => {
+        // if it doesnt have keys just return
+        if (typeof item !== 'object') return item;
+        // otherwise
+        return Object.keys(item).map(key => `${key}: ${item[key]}`).join(', ');
+      }).join(os.EOL);
+    }
+  }
+
+  return data;
+};
+
