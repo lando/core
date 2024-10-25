4a4c148ce2b44df8db6aee7f74d9868de881e080
Mike Pirog <mike@lando.dev>
2024-10-25 07:07:06 -0400

    changed lando setup to pull common plugsin from release channel to better mimic fatcore binary

--- hooks/lando-setup-common-plugins.js
+++ hooks/lando-setup-common-plugins.js
@@ -5,6 +5,14 @@ const merge = require('lodash/merge');
 module.exports = async (lando, options) => {
   // if truthy then skip
   if (options.skipCommonPlugins || options.skipCommonPlugins === 'true' || options.skipCommonPlugins === '1') return;
+
+  // if we are edge then reset common plugins to be edgey
+  if (lando.config.channel == 'edge') {
+    for (const plugin of Object.keys(lando.config.setup.commonPlugins)) {
+      lando.config.setup.commonPlugins[plugin] = 'edge';
+    }
+  }
+
   // otherwise proceed
   lando.log.debug('rebased install plugins %o on common ones %o', options.plugins, lando.config.setup.commonPlugins);
   options.plugins = merge({}, lando.config.setup.commonPlugins, options.plugins);
