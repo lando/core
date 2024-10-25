d48638a2c15b248bddd2ff28cecfe1880ef3b435
Mike Pirog <mike@lando.dev>
2024-10-25 16:10:52 -0400

    improve lando shellenv to produce more consistent newliney output

---
+++ hooks/app-add-path-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = async (app, lando) => {
+  // push a message if the install path is not in the bin
+
+  // console.log(lando.config.cli)
+  // console.log(require('../utils/get-bin-paths')(lando?.config?.cli))
+  // process.exit(1)
+};
