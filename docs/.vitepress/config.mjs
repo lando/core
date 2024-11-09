21d05cbbd2dd54829b89340bdb84d08f8e52359f
Mike Pirog <mike@lando.dev>
2024-11-09 09:11:28 -0500

    updated to @lando/vitepress-theme-default-plus@v1.1.0-beta.19

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   landoPlugin,
   version,
   base: '/',
-  navrel: undefined,
+  navrel: 'none',
   head: [
     ['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1'}],
     ['link', {rel: 'icon', href: '/favicon.ico', size: 'any'}],
