6b0489a882b62a2d46fcb0be1b21d6a5aafcd704
Mike Pirog <mike@lando.dev>
2024-11-21 16:40:17 -0500

    simplify usage of lando shellenv --add

--- utils/get-bin-paths.js
+++ utils/get-bin-paths.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = ({entrypoint, file, installPath}) => {
-  return [installPath, entrypoint, file]
+module.exports = ({installPath}) => {
+  return [installPath]
     .filter(p => typeof p === 'string' && p !== '' && fs.existsSync(p))
     .map(p => !fs.lstatSync(p).isDirectory() ? path.dirname(p) : p)
-    .filter(p => !process.env.PATH.split(path.delimiter).includes(p) || p === installPath)
+    .filter(p => !process.env.PATH.split(path.delimiter).includes(p))
     .filter((v, i, self) => i === self.indexOf(v));
 };
