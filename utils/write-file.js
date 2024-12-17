27ac47158b2bb9485576f34284ef03e6e360c4a3
Mike Pirog <mike@lando.dev>
2024-12-17 11:05:48 -0500

    fix write-file to be more targetted re: ImportString

--- utils/write-file.js
+++ utils/write-file.js
@@ -7,14 +7,13 @@ const path = require('path');
 // @TODO: maybe extension should be in {options}?
 // @TODO: error handling, defaults etc?
 module.exports = (file, data, options = {}) => {
-  // if data is not a string but can be made into a string
-  if (typeof data !== 'string' && typeof data?.toString === 'function') data = data.toString();
-
   // set extension if not set
   const extension = options.extension || path.extname(file);
-
   // linux line endings
   const forcePosixLineEndings = options.forcePosixLineEndings ?? false;
+
+  // special handling for ImportString
+  if (typeof data !== 'string' && data?.name?.constructor === 'ImportString') data = data.toString();
 
   // data is a string and posixOnly then replace
   if (typeof data === 'string' && forcePosixLineEndings) data = data.replace(/\r\n/g, '\n');
