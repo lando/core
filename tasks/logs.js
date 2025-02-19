4c4440db052acb4c9fc746d6d6dd51279edfd0fa
Aaron Feledy <aaron@arrowonesolutions.com>
2025-02-19 13:19:55 -0600

    add --tail to logs command

--- tasks/logs.js
+++ tasks/logs.js
@@ -8,6 +8,7 @@ module.exports = lando => ({
   usage: '$0 logs [--follow] [--service <service>...] [--timestamps]',
   examples: [
     '$0 logs',
+    '$0 logs --tail 10',
     '$0 logs --follow --service appserver',
   ],
   options: {
@@ -22,6 +23,12 @@ module.exports = lando => ({
       alias: ['s'],
       array: true,
     },
+    tail: {
+      describe: 'Number of lines to show from the end of the logs for each service',
+      alias: ['n'],
+      default: 'all',
+      type: 'string',
+    },
     timestamps: {
       describe: 'Shows log timestamps',
       alias: ['t'],
@@ -32,7 +39,7 @@ module.exports = lando => ({
   run: options => {
     // Try to get our app
     const app = lando.getApp(options._app.root);
-    const opts = _.pick(options, ['follow', 'timestamps', 'service']);
+    const opts = _.pick(options, ['follow', 'tail', 'timestamps', 'service']);
     opts.services = opts.service;
     if (app) return app.init().then(() => lando.engine.logs(_.merge(app, {opts})));
   },
